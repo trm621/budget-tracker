390ed7077835147743f00ccaeda34135e534c40c
trm621 <t.maher621@gmail.com>
2022-02-26 22:13:37 -0500

    set up service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,55 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png'
 ];
 
+self.addEventListener('fetch', function (e) {
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(e.request)
+                .then(response => {
+                  if (response.status === 200) {
+                    cache.put(e.request.url, response.clone());
+                  }
+                  return response;
+                })
+                .catch(err => {
+                  return cache.match(e.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+
+      self.addEventListener('activate', function(evt) {
+        evt.waitUntil(
+          caches.keys().then(keyList => {
+            return Promise.all(
+              keyList.map(key => {
+                if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                  console.log('Removing old cache data', key);
+                  return caches.delete(key);
+                }
+              })
+            );
+          })
+        );
+      
+        self.clients.claim();
+      });
+
+e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
+        }
+      });
+    })
+  );
+});
