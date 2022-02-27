bdf7527dde5ce504beecf4b349bf3c87a706b746
trm621 <t.maher621@gmail.com>
2022-02-26 22:49:52 -0500

    tweaking server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const app = express();
 
