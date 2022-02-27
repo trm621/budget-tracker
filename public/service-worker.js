ccf3f74111ca573f761dc5acd35edd59b3a73694
trm621 <t.maher621@gmail.com>
2022-02-26 21:14:00 -0500

    set up files

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'budget-tracker';
+const DATA_CACHE_NAME = 'budget-tracker-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/manifest.json',
+    '/css/styles.css',
+    '/js/idb.js',
+    '/js/index.js',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
+];
+
