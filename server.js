8c4cd13d162ca8f01252875b3fcb72b9397662e6
trm621 <t.maher621@gmail.com>
2026-02-24 22:01:52 -0500

    mongodb

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget-tracker",
+  MONGODB_URI || "mongodb://127.0.0.1:27017/budget-tracker",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
