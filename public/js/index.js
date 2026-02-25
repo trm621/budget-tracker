891e41d05c4e63c89b029f9f9a5ad31dbf81ace4
trm621 <t.maher621@gmail.com>
2026-02-24 23:49:55 -0500

    server

--- public/js/index.js
+++ public/js/index.js
@@ -64,7 +64,7 @@ function sendTransaction(isAdding) {
     transaction.value *= -1;
   }
 
-  transaction.unshift(transaction);
+  transactions.unshift(transaction);
 
   populateChart();
   populateTotal();
